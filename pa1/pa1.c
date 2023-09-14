8c2f6c3b029222937100a3c1101c3e02071eb4ac
SerTitan <apartem08@gmail.com>
2023-09-14 17:43:20 +0300

    Создание файлов логирования

--- pa1/pa1.c
+++ pa1/pa1.c
@@ -3,7 +3,26 @@
 #include "pa1.h"
 #include <stdio.h>
 
+//Файлы логово и сообщений для пайпов
+static const char * const events_log;
+static const char * const pipes_log;
+
 int main() { 
-    printf("Hello world\n"); 
+    FILE *events_file = fopen(events_log, "r");
+    FILE *pipes_file = fopen(pipes_log, "r"); 
+    if (!events_file) {
+         events_file = fopen(events_log, "w");
+        if (events_file)
+            fclose(events_file);
+        else
+            return 1;
+    }
+    if (!pipes_file) {
+         pipes_file = fopen(pipes_log, "w");
+        if (pipes_file)
+            fclose(pipes_file);
+        else
+            return 1;
+    }
     return 0;
 }
