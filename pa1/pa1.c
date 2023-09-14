e1210f58aabd42d072aa43c6bff7bfd309cf0d28
SerTitan <apartem08@gmail.com>
2023-09-14 19:43:05 +0300

    Добавлен ключ -p и его обработка

--- pa1/pa1.c
+++ pa1/pa1.c
@@ -2,12 +2,28 @@
 #include "ipc.h"
 #include "pa1.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <ctype.h>
+#include <string.h>
 
 //Файлы логово и сообщений для пайпов
 static const char * const events_log;
 static const char * const pipes_log;
 
-int main() { 
+int main(int argc, char *argv[]) { 
+    int32_t proc_number = 0;
+    if (argc != 3 || strcmp(argv[1], "-p") != 0 || atoi(argv[2]) == 0){
+        exit(1);
+    }
+    else{
+        proc_number = atoi(argv[2]);
+        // int num_children = atoi(argv[2]);
+        if (proc_number > 10 || proc_number < 0)
+            exit(1);
+    }
     FILE *events_file = fopen(events_log, "r");
     FILE *pipes_file = fopen(pipes_log, "r"); 
     if (!events_file) {
@@ -24,5 +40,10 @@ int main() {
         else
             return 1;
     }
+
+    //pid_t child_pid = fork();
+
+    printf("%d\n" , proc_number);
+    
     return 0;
 }
